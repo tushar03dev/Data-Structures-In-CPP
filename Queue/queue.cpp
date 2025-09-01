fc85169b40c0f35e037acc8c3dc5fa3d9841793c
Tushar Sharma <tushar.sharma29sep@gmail.com>
2025-09-01 14:17:49 +0530

    create queue.cpp

---
+++ Queue/queue.cpp
@@ -0,0 +1,20 @@
+//
+// Created by tushar on 1/9/25.
+//
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+    queue<int> q;
+    q.push(5);
+    cout<<q.size()<<endl;
+    cout<<q.empty()<<endl;
+
+    q.pop();
+    cout<<q.size()<<endl;
+    q.push(10);
+    q.push(20);
+    cout<<q.front()<<endl;
+    cout<<q.back()<<endl;
+}
